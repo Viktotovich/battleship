d0a04b7cc894010a5c8a8a3401a7396ef1237a52
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-11-27 07:35:09 +0400

    "Generated outline for board-randomizer algorithm"

---
+++ src/algorithms/board-randomizer.js
@@ -0,0 +1,19 @@
+const boardRandomizer = {
+  initiate: function (boardObj, ships) {
+    //randomize for both horizontal and vertical placements
+  },
+  randomizeDirections: function () {
+    //return 5 tokens, 1 for each ship. Then delegate horizontal / vertical
+  },
+  verticalCords: function () {
+    //
+  },
+  horizontalCords: function () {
+    //
+  },
+  checkValidity: function () {
+    //check if the space is taken
+  },
+};
+
+export { boardRandomizer };
